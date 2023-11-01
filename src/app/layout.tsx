ba95431579bfab9eb9c2eb1779b04905bff0844d
silvianaim02 <silvianaim68@gmail.com>
2023-11-01 10:07:24 +0700

    feat: add progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { Providers } from '@/components/providers';
 import Footer from '@/components/layout/Footer';
+import NextTopLoader from 'nextjs-toploader';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -24,6 +25,17 @@ export default function RootLayout({
       </head>
       <body className={inter.className}>
         <Providers>
+          <NextTopLoader
+            color='#2299DD'
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing='ease'
+            speed={200}
+            shadow='0 0 10px #2299DD,0 0 5px #2299DD'
+          />
           <Header />
           {children}
           <Footer />
