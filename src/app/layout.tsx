f43bdb38ae218187d2bd876adfe7ba5613a5dd8d
silvianaim02 <silvianaim68@gmail.com>
2023-11-01 14:27:17 +0700

    feat: setup AOS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import Header from '@/components/layout/Header';
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import { Providers } from '@/components/providers';
+import { Providers } from '@/components/provider/providers';
 import Footer from '@/components/layout/Footer';
-import NextTopLoader from 'nextjs-toploader';
+import { AOSInit } from '@/components/aos';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,22 +20,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <head>
-        <link rel='icon' href='/favicon.ico' />
-      </head>
+      <AOSInit />
       <body className={inter.className}>
         <Providers>
-          <NextTopLoader
-            color='#2299DD'
-            initialPosition={0.08}
-            crawlSpeed={200}
-            height={3}
-            crawl={true}
-            showSpinner={false}
-            easing='ease'
-            speed={200}
-            shadow='0 0 10px #2299DD,0 0 5px #2299DD'
-          />
           <Header />
           {children}
           <Footer />
